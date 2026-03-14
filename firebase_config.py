b43e1127d420e821e8122f0fd56043c051f9821c
MIHIR MITHANI <157868082+techavenger123@users.noreply.github.com>
2026-03-14 17:10:19 +0530

    Firebase_config

---
+++ firebase_config.py
@@ -0,0 +1,9 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+cred = credentials.Certificate("firebase-admin_old.json")
+
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
